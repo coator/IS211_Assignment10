07b762e223989e9ba5b62bdc6874dae00f608a17
gamblingrepairman <36687218+gamblingrepairman@users.noreply.github.com>
2020-10-27 14:44:03 -0400

    'finish'

--- query_pets.py
+++ query_pets.py
@@ -6,22 +6,17 @@ def prog():
         choice_str = []
         choice = input('Please select person ID number: ')
         choice_str = choice_str + ["SELECT first_name, last_name, age FROM person WHERE id = {}".format(choice)]
-        choice_str = choice_str + ["SELECT pet.name, pet.breed, pet.age, pet.dead FROM ((pet \
-                                   INNER JOIN person ON person_pet.person_id = person.id)\
-                                   INNER JOIN pet ON person_pet.pet_id = pet.id)\
-                                   WHERE person_id = {}".format(choice)]
+        choice_str = choice_str + ["""SELECT p.name, p.breed, p.age, p.dead 
+                                    FROM pet p
+                                    INNER JOIN person_pet pp
+                                    ON pp.pet_id = p.id
+                                    INNER JOIN person pe
+                                    ON pp.person_id =  pe.id
+                                    WHERE pe.id = {}""".format(choice)]
         if choice == "-1":
             exit()
         else:
             db_query(choice_str)
-
-
-"""SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
-FROM ((Orders
-INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
-INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID)
-WHERE Orders.OrderID =  10248;
-"""
 
 
 def db_query(query_str):
@@ -29,8 +24,12 @@ def db_query(query_str):
     c = conn.cursor()
     for l in query_str:
         c.execute(l)
-        print(c.fetchall())
+        a = c.fetchall()
         conn.commit()
+        if len(a) == 0:
+            print('No object found')
+        else:
+            print(a)
     conn.close()
 
 
